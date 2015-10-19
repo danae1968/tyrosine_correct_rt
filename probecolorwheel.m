e76fbb680e7ed86619a618c4c7dcd899bdf25843
Danae Papadopetraki <d.papadopetraki@student.ru.nl>
2015-10-20 01:25:10 +0200

    faster way to create matlab standar color maps

--- probecolorwheel.m
+++ probecolorwheel.m
@@ -3,25 +3,30 @@ Screen('Preference','SkipSyncTests',1);
 Screen('Preference', 'SuppressAllWarnings', 1);
 [wPtr,rect]=Screen('Openwindow',max(Screen('Screens')));   
 
-myRectThree=[500 500 600 600]
-myRectTwo=[100 100 670 670]
-myRect=[100 100 850 850]
-myRect=CenterRectOnPoint(myRect,rect(3)/2, rect(4)/2);
-myRectTwo=CenterRectOnPoint(myRectTwo,rect(3)/2,rect(4)/2);
+probeColor=[0 0 0];
+myRectThree=[500 500 600 600];
+insideRect=[rect(1) rect(2) 0.67*rect(4) 0.67*rect(4)];
+outsideRect=[rect(1) rect(2) 0.9*rect(4) 0.9*rect(4)];
+outsideRect=CenterRectOnPoint(outsideRect,rect(3)/2, rect(4)/2);
+insideRect=CenterRectOnPoint(insideRect,rect(3)/2,rect(4)/2);
 
 % colors=colormatrix;
-colors=hsvcolormap;
+% load fixedHsv;
+% colors=fixedHsv;
+% colors=hsvcolormap;
+colors=colormap(hsv2(65))*255;
 colorangle=360/length(colors);  %convert into pi? 
 rads=0:colorangle:360;
 
 for i=1:length(colors)
-Screen('FillArc',wPtr,colors(i,:),myRect,rads(i),colorangle);
+Screen('FillArc',wPtr,colors(i,:),outsideRect,rads(i),colorangle);
 end
- Screen('FillOval',wPtr,[255 255 255 ],[myRectTwo]);
-Screen('FrameRect', wPtr, [0 0 0], myRectThree);
+ Screen('FillOval',wPtr,[255 255 255 ],insideRect);
+Screen('FrameRect', wPtr, probeColor, myRectThree);
 Screen('Flip',wPtr)
 [clicks,x,y,whichButton] = GetClicks(wPtr)
 % KbStrokeWait();
 % for  j=1:clicks
 %  clut=Screen('GetClut',wPtr,8)
 clear Screen
+
